c7becd0088d89a0e8618864cb57aa6b795904ee6
Bogdan Doicin <alex.zanardi@gmail.com>
2019-04-26 18:14:40 +0300

    Perfecting OAuth connection

--- Form1.cs
+++ Form1.cs
@@ -6,15 +6,16 @@ namespace HT_Match_Predictor
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
+
+            //todo sa descopar o cale de a ma conecta fara a mai cere acele tokens, daca nu e prima data cand ma conectez
+            //todo am reusit sa fac conexiunea cu serverul, am cerut un fisier acum sa vad cum intru un posesia lui
+
             Manager o = new Manager();
             o["consumer_key"] = "2BkDvCeUZL1nCIVOn5KhUb";
             o["consumer_secret"] = "PvSRGYlTxCwUKuw9BH9CIWP1AqutO9MB2JRDGHsVlGC";
-            //o["token"] = "NGcEpaQg2I4QYLWy";
-            //o["token_secret"] = "riboptAJpTEUadcr";
             OAuthResponse rt = o.AcquireRequestToken("https://chpp.hattrick.org/oauth/request_token.ashx", "GET");
             var url = "https://chpp.hattrick.org/oauth/authorize.aspx?oauth_token=" + o["token"];
             System.Diagnostics.Process.Start(url);
@@ -27,10 +28,9 @@ namespace HT_Match_Predictor
             request.PreAuthenticate = true;
             request.AllowWriteStreamBuffering = true;
             request.Headers.Add("Authorization", authzHeader);
-            var response = (HttpWebResponse)request.GetResponse();
-            MessageBox.Show(response.StatusCode.ToString());
-            System.Diagnostics.Process.Start(search);
+            request.ServicePoint.Expect100Continue = false;
+            request.ContentType = "x-www-form-urlencoded";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
         }
-
     }
 }
